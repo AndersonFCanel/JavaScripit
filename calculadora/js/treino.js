77a632c8316c8af1a20cb9a7c465b8f15d614b36
Anderson Ferreira Canel <32991782+AndersonFCanel@users.noreply.github.com>
2017-12-06 15:35:25 -0200

    Update treino.js

--- calculadora/js/treino.js
+++ calculadora/js/treino.js
@@ -1,38 +1,55 @@
-function calculadora(){
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html>
+<head>
+      <title>JavaScript 23</title>
+      <script type="text/javascript" src="js/treino.js"> </script>
+	  <style text="text/css">
+	     img, table { width:250px; }
+	     fieldset {width:250px; }
+		 label {display:block; float:left; }
+		 label, input { width:80px; margin:3px 0;}
+		 th, td {border:1px solid #000; font-size:0.8em; }
+	  </style>
+</head>
+
+<body>
+	
+<form id="formulario"> 
+    <fieldset>
+        <legend>Calculadora </legend>
+        		
+        <label for="op1">Primeiro Operando:</label><br />
+		<input type="number" name="op1" /><br>
+		<br />
+		
+		<label for="Op">Operação:</label>
+		<input id="op" type="text" name="op" /><br>
+		
+		<label for="op2">Segundo Operando:</label><br />
+		<input type="number" name="op2" /><br>
+		<br />
+		
+		<label for="r">Resultado:</label>
+		<input type="text" name="r" disabled="disabled" /> <br>
+		
+		<a href="#" onclick="calculadora()";>Calcular</a>
+	</fieldset>
+</form>
+
+<br>
 
 
-	var formulario = document.getElementById("formulario");
-	
-	var op1 = +formulario.op1.value;
-	var op2 = +formulario.op2.value;
-	var op = formulario.op.value; 
-	
-	
-	
-	var resultado;
-	
-	switch(op){
-		case '+':
-			resultado = op1 + op2;
-			//formulario.r.value = r.toFixed(2);
-			alert(op1 + " + " + op2 + " = " +resultado);
-		break;
-		case '-':
-			resultado = op1 - op2;
-			//formulario.r.value = r.toFixed(2);
-			alert(op1 + " - " + op2 + " = " +resultado);
-		break;
-		case '*':
-			resultado = op1 * op2;
-			//formulario.r.value = r.toFixed(2);
-			alert(op1 + " * " + op2 + " = " +resultado);	
-		break;
-		case '/':
-			resultado = op1 / op2;
-			//formulario.r.value = r.toFixed(2);
-			alert(op1 + " / " + op2 + " = " +resultado);
-		break;
-	}
-	
-	
-}
+<table>
+	 <tr><td align= "center" > INSTRUÇÕES:</td> </tr>
+</table>	 
+<table>
+     <tr><th>SINAIS</th><th>OPERAÇÕES</th></tr>
+	 <tr><td align= "center" >+</td><td>Para soma</td></tr>
+	 <tr><td align= "center">-</td><td>Para Subtração</td></tr>
+	 <tr><td align= "center">*</td><td>Para Multiplicação</td></tr>
+	 <tr><td align= "center">/</td><td>Para Divisão</td></tr>
+</table>
+</body>
+
+</html>
